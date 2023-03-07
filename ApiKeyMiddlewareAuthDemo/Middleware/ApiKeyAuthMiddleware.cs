3f55527e70ef056b8136c56d240f5957e920c4d9
Brian Fontana <brian@codefoxtrot.com>
2023-03-07 17:57:06 -0500

    Add ApiKeyMiddlewareAuthDemo project
    This approach covers the scenario of using an API key, implemented as middleware. The limitation being, is that it applies to your entire API, and may not be as granular as you'd like it to be.

---
+++ ApiKeyMiddlewareAuthDemo/Middleware/ApiKeyAuthMiddleware.cs
@@ -0,0 +1,48 @@
+﻿using ApiKeyMiddlewareAuthDemo.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Primitives;
+using Microsoft.Net.Http.Headers;
+
+namespace ApiKeyMiddlewareAuthDemo.Middleware;
+
+public class ApiKeyAuthMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly IConfiguration _config;
+
+    public ApiKeyAuthMiddleware(RequestDelegate next, IConfiguration configuration)
+    {
+        _next = next;
+        _config = configuration;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        List<ApiKeyModel> validKeys = _config.GetSection("ApiKeys").Get<List<ApiKeyModel>>();
+
+        KeyValuePair<string, StringValues> apiKeyHeader = context.Request.Headers
+            .FirstOrDefault(h => validKeys.Select(
+                x => x.HeaderName.ToLower()).Contains(h.Key.ToLower()));
+
+        if (apiKeyHeader.Equals(default(KeyValuePair<string, StringValues>)))
+        {
+            context.Response.StatusCode = 401;
+            await context.Response.WriteAsync("Missing API key");
+            return;
+        }
+
+        ApiKeyModel matchingKey = validKeys
+            .Where(x => x.HeaderName.ToLower().Equals(apiKeyHeader.Key.ToLower()))
+            .Where(x => x.ApiKey.ToLower().Equals(apiKeyHeader.Value[0].ToLower()))
+            .FirstOrDefault();
+
+        if (matchingKey is null)
+        {
+            context.Response.StatusCode = 401;
+            await context.Response.WriteAsync("Invalid API key");
+            return;
+        }
+
+        await _next(context);
+    }
+}
---
+++ ApiKeyMiddlewareAuthDemo/Program.cs
@@ -0,0 +1,92 @@
+using ApiKeyMiddlewareAuthDemo.Middleware;
+using Microsoft.OpenApi.Models;
+using System.Text.Json.Serialization;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers().AddJsonOptions(config =>
+{
+    config.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "WebApi v1",
+        Version = "v1",
+        Description = "This is a demo API"
+    });
+    //options.SwaggerDoc("v2", new OpenApiInfo
+    //{
+    //    Title = "WebApi v2",
+    //    Version = "v2",
+    //    Description = "This is a demo API"
+    //});
+    options.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
+    {
+        Name = "X-API-KEY",
+        Type = SecuritySchemeType.ApiKey,
+        In = ParameterLocation.Header,
+        Description = "API key authorization header",
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "ApiKey"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+builder.Services.AddApiVersioning(options =>
+{
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.DefaultApiVersion = new(1, 0);
+    options.ReportApiVersions = true;
+});
+builder.Services.AddVersionedApiExplorer(options =>
+{
+    options.GroupNameFormat = "'v'VVV";
+    options.SubstituteApiVersionInUrl = true;
+});
+builder.Services.AddCors(policy =>
+{
+    policy.AddPolicy("OpenCorsPolicy", options =>
+        options
+            .AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod());
+});
+WebApplication app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        // options.SwaggerEndpoint("/swagger/v2/swagger.json", "WebApi v2");
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApi v1");
+        options.EnableTryItOutByDefault();
+        options.ConfigObject.AdditionalItems["syntaxHighlight"] = new Dictionary<string, object>
+        {
+            ["activated"] = false
+        };
+    });
+}
+
+app.UseHttpsRedirection();
+app.UseCors("OpenCorsPolicy");
+// Add the middleware here. The limitation of this approach,
+// is that it covers EVERYTHING in the API with the logic
+// found in the middleware.
+app.UseMiddleware<ApiKeyAuthMiddleware>();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
